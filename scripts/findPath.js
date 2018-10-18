ef7a61f122279e1b49989c569a78abbd91d26a1a
Jim Blackler <jimblackler@gmail.com>
2018-10-19 00:34:54 +0100

    Fixes to path finding.

--- scripts/findPath.js
+++ scripts/findPath.js
@@ -1,17 +1,34 @@
 export function findPath(edgePairs, nodes) {
-  const path = [];
-  let node =  nodes.entries().next().value[0];
-  while (true) {
-    nodes.delete(node);
-    path.push(node);
-    if (nodes.size == 0) {
-      return path;
-    }
-    for (const neighbour of edgePairs.get(node)) {
-      if (nodes.has(neighbour)) {
-        node = neighbour;
-        break;
+  const firstNode =  nodes.entries().next().value[0];
+
+  let candidates = new Set();
+
+  candidates.add([[firstNode], nodes]);
+
+  while(true) {
+    const next = new Set();
+    for (const candidate of candidates) {
+      const path = candidate[0];
+      const nodes = candidate[1];
+      const lastNode = path[path.length - 1];
+      for (const neighbour of edgePairs.get(lastNode)) {
+        if (!nodes.has(neighbour)) {
+          continue;
+        }
+        const newNodes = new Set(nodes);
+        newNodes.delete(neighbour);
+        if (newNodes.size == 0) {
+          if (neighbour == firstNode) {
+            return path;
+          }
+        } else {
+          next.add([path.concat(neighbour), newNodes]);
+        }
       }
     }
+    if (next.size == 0) {
+      return null;
+    }
+    candidates = next;
   }
 }
--- scripts/findPerimeter.js
+++ scripts/findPerimeter.js
@@ -4,13 +4,7 @@ export function getPerimeterNodes(edgePairs) {
   const perimeterNodes = new Set();
 
   for (let [node, neighbours] of edgePairs) {
-    let neighbourCoEdges = 0;
-    for (const neighbourNode of neighbours) {
-      const neighbourNeighbours = edgePairs.get(neighbourNode);
-      const intersection = new Set([...neighbourNeighbours].filter(x => neighbours.has(x)));
-      neighbourCoEdges += intersection.size;
-    }
-    if (neighbourCoEdges < neighbours.size * 2) {
+    if (!findPath(edgePairs, neighbours)) {
       perimeterNodes.add(node);
     }
   }
